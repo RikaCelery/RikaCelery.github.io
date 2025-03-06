034ef1968b70927f681df288d73caa8955fabde3
RikaCelery <qincai233@gmail.com>
2025-03-06 21:42:37 +0800

    feat(newpost.sh): add category support and improve script
    - Add support for creating posts in specific categories
    - Improve variable handling with curly braces for clarity
    - Add sed command to update category in the post metadata
    - Update file path handling to accommodate category folders

--- newpost.sh
+++ newpost.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 category=$2
 
-if [[ -z $category ]]; then
+if [[ -z ${category} ]]; then
   category=""
 fi
 if [[ -z $1 ]]; then
   echo name not specific
   exit 1
 fi
-hugo new content/post/"$category""$1"/index.md
-code content/post/"$category""$1"/index.md
+hugo new content/post/"${category}"/"$1"/index.md
+# exist category
+if [[ -n ${category} ]]; then
+  sed -i "" "s/未分类/${category}/g" "content/post/${category}/$1/index.md"
+fi
+code "content/post/${category}/$1/index.md"
